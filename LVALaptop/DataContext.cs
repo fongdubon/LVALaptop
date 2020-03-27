20a04ed4b36fe1a41798f035ca0d34666b763dd9
Giovanni Rodríguez <giovannii@outlook.com>
2020-03-27 12:32:13 -0600

    Agregar DataC ontext

---
+++ LVALaptop/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using LVALaptop.Data;
+using System.Data.Entity;
+
+namespace LVALaptop
+{
+    public class DataContext: DbContext
+    {
+        public DbSet <Employee> Employees { get; set;}
+        public DbSet<Portable> Portables { get; set; }
+
+        public DbSet <EmployeePortable> EmployeePortables { get; set;}
+
+    }
+}
