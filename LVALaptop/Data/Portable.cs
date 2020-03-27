fc4fef0874e3372a3e259d0523333a9bf3cf6e48
EdwinLR <edwin20.elr@gmail.com>
2020-03-27 11:47:16 -0600

    Respuesta al comentario

--- LVALaptop/Data/Portable.cs
+++ LVALaptop/Data/Portable.cs
@@ -3,6 +3,7 @@
     using System;
     public class Portable : IEntity
     {//hola
+        //Hola Luis
         public int Id { get; set ; }
         public string SerialNumber { get; set; }
         public string Model { get; set; }
